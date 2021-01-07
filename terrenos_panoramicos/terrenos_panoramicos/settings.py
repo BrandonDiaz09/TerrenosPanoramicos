0c3666750d0ea4f27c3291b3a047c2f374ecc51e
Axe D W <axtherocknass@gmail.com>
2021-01-06 21:07:17 -0600

    agregando lo de bases de datos

--- terrenos_panoramicos/terrenos_panoramicos/settings.py
+++ terrenos_panoramicos/terrenos_panoramicos/settings.py
@@ -79,9 +79,13 @@ WSGI_APPLICATION = 'terrenos_panoramicos.wsgi.application'
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+   'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'postgres',
+        'USER': 'postgres',
+        'PASSWORD': 'TCHALLA&chiquita2580',
+        'HOST': '35.192.199.27',
+        'PORT': '',
     }
 }
 
